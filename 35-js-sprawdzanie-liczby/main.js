8f0f65e35bde928049fa0ec7db98eeba116afccf
Adrianna <lisicka.adrianna@gmail.com>
2023-04-07 20:32:36 +0200

    adding function on the interval

---
+++ 35-js-sprawdzanie-liczby/main.js
@@ -0,0 +1,8 @@
+function myFunction(n) {
+  if (n >= 100 && n <= 200) {
+    console.log("Liczba znajduje się w przedziale");
+  } else {
+    console.log("Liczba nie znajduje się w przedziale");
+  }
+}
+myFunction(120);
