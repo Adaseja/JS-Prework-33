e738c83b95713f8b41404c9645bf03d72c2187a6
adaseja <106311865+Adaseja@users.noreply.github.com>
2023-04-14 14:30:35 +0200

    moving folders

---
+++ JS3-25-js-tworzenie-tablicy/main.js
@@ -0,0 +1,13 @@
+/* Zadanie 25-js-tworzenie-tablicy */
+
+let numbers = [2, 4, 6, 8, 10];
+
+/* Zadanie 26-js-tablica-ze-zmiennymi */
+
+let number1 = 1;
+
+let number2 = 2;
+
+let number3 = 3;
+
+let array = [number1, number2, number3];
---
+++ js1-23-js-deklaracja-zmiennych/main.js
@@ -0,0 +1,3 @@
+let firstName = "Adrian";
+
+let lastName = "Lis";
